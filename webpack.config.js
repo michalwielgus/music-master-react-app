30e43bd6303388066bb2eb45df3b7a3baafc933d
Michał Wielgus <wielgus.michaal@gmail.com>
2018-02-01 08:26:34 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var react = require('react')
+
+module.exports = {
+    entry: ['./src/main.js'],
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                query: {
+                    presets: ['env', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './build',
+        inline: true
+    }
+}
